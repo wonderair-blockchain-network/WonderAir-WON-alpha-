8cd68e8ce53204c7d797a4b6a9f1922f1a024eb6
WonderAir <49903007+WonderAir@users.noreply.github.com>
2019-06-21 11:27:29 +0800

    Add files via upload

---
+++ WonderAir whisperv2/config.go
@@ -0,0 +1,18 @@
+// Copyright (c) 2018 The wonderair ecosystem Authors
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or or or http://www.opensource.org/licenses/mit-license.php
+
+
+package whisperv6
+
+// Config represents the configuration state of a whisper node.
+type Config struct {
+	MaxMessageSize     uint32  `toml:",omitempty"`
+	MinimumAcceptedPOW float64 `toml:",omitempty"`
+}
+
+// DefaultConfig represents (shocker!) the default configuration.
+var DefaultConfig = Config{
+	MaxMessageSize:     DefaultMaxMessageSize,
+	MinimumAcceptedPOW: DefaultMinimumPoW,
+}
