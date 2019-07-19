9764d3dfb1b2e5cfa6779ba8ab8f92786a859dab
WonderAir <49903007+WonderAir@users.noreply.github.com>
2019-07-19 12:00:03 +0800

    Add files via upload

---
+++ errors.go
@@ -0,0 +1,24 @@
+// Copyright (c) 2018 The wonderair ecosystem Authors
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or or or http://www.opensource.org/licenses/mit-license.php
+
+
+package trie
+
+import (
+	"fmt"
+
+	"github.com/wonderair ecosystem/go-wonderair ecosystem/common"
+)
+
+// MissingNodeError is returned by the trie functions (TryGet, TryUpdate, TryDelete)
+// in the case where a trie node is not present in the local database. It contains
+// information necessary for retrieving the missing node.
+type MissingNodeError struct {
+	NodeHash common.Hash // hash of the missing node
+	Path     []byte      // hex-encoded path to the missing node
+}
+
+func (err *MissingNodeError) Error() string {
+	return fmt.Sprintf("missing trie node %x (path %x)", err.NodeHash, err.Path)
+}
